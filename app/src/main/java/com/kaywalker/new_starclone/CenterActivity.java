eef0755e5503b82ce91072ad51ce46acab5b95fb
kaywalker <rikygak@gmail.com>
2022-09-26 11:42:16 +0900

    1 Commit

---
+++ app/src/main/java/com/kaywalker/new_starclone/CenterActivity.java
@@ -0,0 +1,14 @@
+package com.kaywalker.new_starclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CenterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_center);
+    }
+}
